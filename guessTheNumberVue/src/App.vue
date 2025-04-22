7bed3cf9bb38419628293003c504e6e6286a057d
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-22 15:05:15 +0200

    Initial commit

---
+++ guessTheNumberVue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import GuessNumber from './components/GuessNumber.vue'
+
+</script>
+
+<template>
+  <div>
+    <GuessNumber text="Tipp-tallet-spillet"></GuessNumber>
+   
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ guessTheNumberVue/src/components/GuessNumber.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ text: string }>()
+
+const secretNumber = ref(Math.ceil(Math.random() * 100))
+const guessNumber = ref<number | null>(null)
+
+function newGame(){
+  secretNumber.value = Math.ceil(Math.random() * 100)
+}
+</script>
+
+<template>
+  <h2>{{text}}</h2>
+  <p>
+    {{ secretNumber }}
+  </p>
+  <p>
+    Your guess {{ guessNumber }}
+  </p>
+  <p v-if="guessNumber < secretNumber">To Low</p>
+  <p v-else-if="guessNumber > secretNumber">To High</p>
+  <p v-else>You are correct</p>
+  
+  <!-- <button @click="guess()">Tipp</button>       -->
+  <input type="number" v-model.number="guessNumber" />
+  <button @click="newGame()">Start new game</button>  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
