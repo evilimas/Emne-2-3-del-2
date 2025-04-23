b13e92cf450c4dc201dcd4f743043045e00f0397
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-23 13:10:03 +0200

    hangman spill

---
+++ HangmanVue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hangman from './components/Hangman.vue'
+</script>
+
+<template>
+  <Hangman msg="Hangman game" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ HangmanVue/src/components/Hangman.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const secretText = ref<string>('TEE')
+const currentText = ref<string>('___')
+const guessedLetter = ref<string>('')
+
+
+function guess(){
+  currentText.value = evaluateHangman(secretText.value, currentText.value, guessedLetter.value);
+  guessedLetter.value = ''
+}
+
+        
+function evaluateHangman(secretText : string, currentText : string, guessedLetter : string) : string {
+  const index = secretText.indexOf(guessedLetter);
+  if (index == -1) return currentText;
+  const currentTextBeforeGuessedLetter = currentText.substring(0, index);
+  const currentTextAfterGuessedLetter = currentText.substring(index + 1);
+  const secretTextAfterGuessedLetter = secretText.substring(index + 1);
+  return currentTextBeforeGuessedLetter + guessedLetter
+  + evaluateHangman(secretTextAfterGuessedLetter, currentTextAfterGuessedLetter, guessedLetter);
+}
+
+function newGame(){
+  secretText.value = 'COP'
+  currentText.value = '___'
+
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <h1>{{currentText}}</h1>
+  <div v-if="secretText==currentText">
+    Løst! <button @click="newGame()">Nytt spill</button>
+  </div>
+    <input type="text" v-model="guessedLetter" maxlength="1" placeholder="Write only one letter"/>
+    <button @click="guess()">Guess</button>
+
+  
+</template>
+
+<style scoped>
+
+</style>
