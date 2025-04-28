1ddfaa6dbcf258db89d9119cbe18bfe868e8a4a5
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-28 13:08:19 +0200

    init

---
+++ todoVue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ToDos from "@components/ToDos.vue"
+</script>
+
+<template>
+  <div class="app w3-blue-gray">
+    <ToDos />
+  </div>
+</template>
+
+<style scoped>
+
+.app {
+ display: flex;
+ justify-content: center;
+ width: 100vw;
+ min-height: 100vh;
+ padding: 5rem;
+}
+</style>
